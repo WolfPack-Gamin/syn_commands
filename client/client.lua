24ad087ea1fabe45c9489496c0a2d9a989c8374b
JusCampin <37031311+JusCampin@users.noreply.github.com>
2022-09-20 20:41:42 -0700

    Initial commit

---
+++ client/client.lua
@@ -0,0 +1,41 @@
+local player = PlayerPedId()
+
+RegisterCommand("Stinky", function(rawCommand)
+    TaskPlayEmoteWithHash(player, 0, 0, -166523388, 1, 1, 0, 0)
+end)
+
+RegisterCommand("tip", function(rawCommand)
+    TaskPlayEmoteWithHash(player, 0, 0, -1457020913, 1, 1, 0, 0)
+end) --TipHat
+
+RegisterCommand("Slit", function(rawCommand)
+    TaskPlayEmoteWithHash(player, 0, 0, 1256841324, 1, 1, 0, 0)
+end) --Slit Throat
+
+RegisterCommand("SlwClp", function(rawCommand)
+    TaskPlayEmoteWithHash(player, 0, 0, 1023735814, 1, 1, 0, 0)
+end) --Slow Clap
+
+RegisterCommand("Smh", function(rawCommand)
+    TaskPlayEmoteWithHash(player, 0, 0, -653113914, 1, 1, 0, 0)
+end) --smh
+
+RegisterCommand("Spit", function(rawCommand)
+    TaskPlayEmoteWithHash(player, 0, 0, -2106738342, 1, 1, 0, 0)
+end) --Spit
+
+RegisterCommand("No", function(rawCommand)
+    TaskPlayEmoteWithHash(player, 0, 0, 1509171361, 1, 1, 0, 0)
+end) --Thumbs Down
+
+RegisterCommand("Yes", function(rawCommand)
+    TaskPlayEmoteWithHash(player, 0, 0, 425751659, 1, 1, 0, 0)
+end) --Thumbs Up
+
+RegisterCommand("Wave", function(rawCommand)
+    TaskPlayEmoteWithHash(player, 0, 0, -339257980, 1, 1, 0, 0)
+end) --wave
+
+RegisterCommand("Bird", function(rawCommand)
+    TaskPlayEmoteWithHash(player, 0, 0, 969312568, 1, 1, 0, 0)
+end) --smh
